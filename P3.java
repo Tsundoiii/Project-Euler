06de936cd737d86e9d392c08d4378870a027de82
Tsundoiii <o2oxo0dbv@relay.firefox.com>
2022-12-04 13:34:38 -0500

    p3

--- P3.java
+++ P3.java
@@ -1,19 +1,20 @@
 public class P3 {
-    static boolean isPrime(int n) {
-        for (int i = 2; i < n; i++) {
+    public static void main(String[] args) {
+        long largestFactor = 0;
+        for (long i = 2; i < Math.sqrt(600851475143L); i++) {
+            if (600851475143L % i == 0 && i > largestFactor && isPrime(i)) {
+                System.out.println(i);
+                largestFactor = i;
+            }
+        }
+        System.out.println(largestFactor);
+    }
+    static boolean isPrime(long n) {
+        for (long i = 2; i < n; i++) {
             if (n % i == 0) {
                 return false;
             }
         }
         return true;
-    }
-    public static void main(String[] args) {
-        long largestFactor = 0;
-        for (long i = 2; i < 600851475143L; i++) {
-            if (600851475143L % i == 0 && i > largestFactor) {
-                largestFactor = i;
-            }
-        }
-        System.out.println(largestFactor);
     }
 }
